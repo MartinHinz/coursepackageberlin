e313b2e7bae4f586ee684d8138bd5c0fd2f68b89
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-20 10:07:56 +0200

    added packrat, removed magittr dependency

--- R/analytical_functions.R
+++ R/analytical_functions.R
@@ -23,8 +23,7 @@ sample_letters <- function(x) {sample(letters, size = x, replace = T)}
 #'
 #' @export
 make_lorem_ipsum <- function(words_count) {
-  sapply( 1:words_count,function(x) {
-    sample(1:10, size=1) %>% sample_letters %>%
-      paste(collapse ="")
-  }) %>% paste(collapse = " ")
+  paste(sapply( 1:words_count,function(x) {
+    paste(sample_letters(sample(1:10, size=1)),collapse ="")
+  }),collapse = " ")
 }
